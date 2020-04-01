b9753fe14abde4163a7878f9953f69c12d9e50ee
Kc Kai <kirstenckai@gmail.com>
2020-04-01 20:54:30 +0000

    Minor fix to property_reviews id

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -42,8 +42,8 @@ CREATE TABLE reservations (
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
